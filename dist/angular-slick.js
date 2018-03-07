657634ab647f84bb9bc7ecb375a67d64a9c5ab33
Iulian Miu <iulianmiu@Iulians-MacBook-Pro.local>
2018-03-07 12:15:10 +0200

    mod

--- dist/angular-slick.js
+++ dist/angular-slick.js
@@ -140,7 +140,7 @@ angular
           init = function () {
             initOptions();
 
-            var slickness = angular.element(element);
+            var slickness = $(element);// angular.element(element);
 
             if (angular.element(element).hasClass('slick-initialized')) {
               if (options.enabled) {
